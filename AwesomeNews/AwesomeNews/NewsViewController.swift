de08b62a2dda556eedab0f7de21088dfe07c4eba
Samsonov Roman <samson-ra@yandex.ru>
2025-07-09 15:26:59 +0300

    Создал project AwesomeNews + .gitignore

---
+++ AwesomeNews/AwesomeNews/NewsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AwesomeNews
+//
+//  Created by Samson on 09.07.2025.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
